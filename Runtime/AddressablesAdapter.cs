3ad2395ea9d857fd338b36ff0595f8652406838f
Bli_Aik <haikun2333@126.com>
2025-09-08 13:37:05 +0800

    chore(addressables): update assembly references and namespaces
    Update assembly definition precompiled references from Lunar.dll to Lunar.Core.Base.dll and Lunar.Core.ECS.dll. Update namespace from Lunar.Interfaces to Lunar.Core.Base.Interfaces in AddressablesAdapter.cs to align with new assembly structure.

--- Runtime/AddressablesAdapter.cs
+++ Runtime/AddressablesAdapter.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Lunar.Interfaces;
+using Lunar.Core.Base.Interfaces;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using Object = UnityEngine.Object;
 
