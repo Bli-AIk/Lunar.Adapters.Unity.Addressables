61d00db14dafb8982975fe4cbf83c92af1b1fe9c
AIk <111052929+Bli-AIk@users.noreply.github.com>
2025-09-18 13:27:13 +0800

    v0.1.0: feat: Initialize the project (#1)
    * Initial import: Lunar.Adapters.Unity.Addressables
    
    * feat(addressables): add Addressables adapter package
    
    * fix(ResourcesAdapter): correct parameter name in Release method
    
    fix(AddressablesAdapter): make LoadAllAsync methods static
    test(AddressablesAdapter): update tests for static LoadAllAsync
    chore: update demo script and IDE config
    
    * chore(asmdef): Update assembly definition references
    
    Changed overrideReferences to true and added Lunar.dll to precompiledReferences.

---
+++ Tests/AddressablesAdapterTypeValidationTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Lunar.Adapters.Unity.Addressables.Tests
+{
+    public class AddressablesAdapterTypeValidationTests
+    {
+        private AddressablesAdapter _addrAdapter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _addrAdapter = new AddressablesAdapter();
+        }
+
+        [Test]
+        public void LoadAsync_ThrowsImmediately_ForNonUnityType()
+        {
+            // Should throw before any Addressables call because of type validation
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                // We just call the method; it should synchronously throw
+                var _ = _addrAdapter.LoadAsync<int>("someKey");
+            });
+        }
+
+        [Test]
+        public void LoadAllAsync_StringPath_ThrowsImmediately_ForNonUnityType()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                // static overload also validates type right away
+                Task.Run(() => AddressablesAdapter.LoadAllAsync<int>("somePath", null,
+                    UnityEngine.AddressableAssets.Addressables.MergeMode.None,
+                    CancellationToken.None));
+            });
+        }
+
+        [Test]
+        [Obsolete("Obsolete")]
+        public void Obsolete_LoadAll_Static_ThrowsImmediately_ForNonUnityType()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                AddressablesAdapter.LoadAll<int>("somePath", null);
+            });
+        }
+    }
+}
